26ea7bc233f81c7581d99bfe6b27a89ef2ce4396
KozachukAP <aleksandr.kozachuk@stm-labs.ru>
2024-09-11 11:45:36 +0300

    main:

--- src/guard/gptadapter/core/component/yandex_gpt_api.py
+++ src/guard/gptadapter/core/component/yandex_gpt_api.py
@@ -1,6 +1,10 @@
-from yandex_gpt import YandexGPT, YandexGPTConfigManagerForIAMToken
+from yandex_gpt import YandexGPT, YandexGPTConfigManagerForIAMToken, YandexGPTConfigManagerForAPIKey
+from decouple import config
 
-config = YandexGPTConfigManagerForIAMToken()
+model_type = config("YANDEX_GPT_MODEL_TYPE")
+catalog_type_id = config("YANDEX_GPT_CATALOG_ID")
+api_key = config("YANDEX_GPT_API_KEY")
+config = YandexGPTConfigManagerForAPIKey(model_type=model_type, catalog_id=catalog_type_id, api_key=api_key)
 
 yandex_gpt = YandexGPT(config_manager=config)
 
